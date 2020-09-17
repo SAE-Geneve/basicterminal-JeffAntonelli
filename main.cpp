e8634bae61be8b331ae666cd8bb3086f375b3ce3
Jeffrey Antonelli <antonellijeffrey@gmail.com>
2020-09-17 13:30:15 +0200

    Salut

--- main.cpp
+++ main.cpp
@@ -1,6 +1,34 @@
-#include <windows.h>
+#include <string>
+#include <iostream>
 
 int main()
 {
-	MessageBox(nullptr, "World!", "Hello", MB_ICONEXCLAMATION);
+	/*const int value = 3 ;
+	std::string apple_count =
+		std::to_string(value) +
+		"apples";
+	std::cout
+		<< "you have "
+		<<  apple_count;
+
+	std::string txt = "apple_count";
+	std::cout
+		<< " size: "
+		<< std::to_string(txt.length());*/
+
+	/*std::string txt = "here is something";
+	char value = txt[6];
+		std::cout
+		<< "The character is ["
+		<< value
+		<< "]\n";*/
+	std::cout << "Enter your first name:";
+	//std::string first_name;
+		//std::cin >> first_name;
+		//std::cout << first_name; //pour afficher le nom
+	std::string name;
+	std::getline(std::cin, name);
+	std::cout <<name;
+	return 0;
 }
+
